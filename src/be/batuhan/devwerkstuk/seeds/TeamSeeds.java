a64fe56c74a228fe652260474cced1a6e3e931bf
batuhanayazz <43605933+batuhanayazz@users.noreply.github.com>
2021-05-09 01:48:10 +0200

    Added temporary seeds

---
+++ src/be/batuhan/devwerkstuk/seeds/TeamSeeds.java
@@ -0,0 +1,5 @@
+package be.batuhan.devwerkstuk.seeds;
+
+public class TeamSeeds {
+    public static String[] names = {"Galatasaray", "Genk", "Antwerpen", "Anderlecht", "Fenerbahce", "Konyaspor"};
+}
